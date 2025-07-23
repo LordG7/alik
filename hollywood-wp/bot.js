5b2582ac0d5c653ed147d91a7af9b332432796b6
unknown <izzetomarovcff@gmail.com>
2025-07-23 04:35:31 +0400

    initial commit start

---
+++ hollywood-wp/bot.js
@@ -0,0 +1,19 @@
+const { Telegraf, session, Markup } = require('telegraf');
+require('dotenv').config();
+const token = process.env.TOKEN
+const bot = new Telegraf(token); // Use config_data for better security
+bot.use(session());
+bot.start((ctx) => {
+  ctx.reply(
+    'Contact me:',
+    Markup.inlineKeyboard([
+      Markup.button.url('📱 Contact Me', 'https://wa.me/79232874777'),
+    ])
+  );
+});
+bot.launch();
+console.log('Bot is running...');
+
+// Graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
